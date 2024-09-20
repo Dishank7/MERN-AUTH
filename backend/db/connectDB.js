e2d9626906ee235ddf782c9f635183d36a78e902
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2024-09-20 23:03:12 +0530

    sec changes

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
 	try {
-		console.log("mongo_uri: ", process.env.MONGO_URI);
+		console.log("mongo_uri:", process.env.MONGO_URI);
 		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
